a780b30402fd36a9e9f6b858ba9be8f7ebada81b
Arne Swanson <atteinmos@0111.click>
2018-07-10 18:57:43 -0400

    fixed anim times

--- main.js
+++ main.js
@@ -2,18 +2,15 @@ const typed = new Typed("#typed", {
 	stringsElement: "#typed-strings",
 	typeSpeed: 40,
 	showCursor: false,
-	startDelay: 2200,
+	startDelay: 2000,
 	backSpeed: 20,
 	smartBackspace: true
 });
 
 setTimeout(() => {
 	$('#logo').fadeOut(200)
+	$('#content').fadeIn(1)
 }, 2000)
-
-setTimeout(() => {
-	$('#content').fadeIn(200)
-}, 2200)
 
 $(() => {
   $(".right").hide(0).delay(7125+2000).fadeIn(400);
