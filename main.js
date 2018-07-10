c87500078dc921a5d2d02ed43f4c55694eb038a0
Arne Swanson <atteinmos@0111.click>
2018-07-10 18:00:48 -0400

    added milo

---
+++ main.js
@@ -0,0 +1,16 @@
+var typed
+setInterval(() => {
+	$('#content').fadeIn(1)
+	$('#logo').fadeOut(200)
+	typed = new Typed("#typed", {
+		stringsElement: "#typed-strings",
+		typeSpeed: 40,
+		backSpeed: 20,
+		smartBackspace: true
+	});
+}, 2000)
+
+$(function() {
+  $(".right").hide(0).delay(7125+200).fadeIn(400);
+  // $(".logo").hide(0).delay(7500).fadeIn(400);
+});
