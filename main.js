f660535ed10b69077debc94f804d44212985199e
frogheck <frogheck@gmail.com>
2018-07-10 18:33:39 -0400

    aa

--- main.js
+++ main.js
@@ -1,21 +1,13 @@
 var typed = new Typed("#typed", {
   stringsElement: "#typed-strings",
   strings: ["NOKOI is a game studio.", "NOKOI is a collective."],
-<<<<<<< HEAD
   startDelay: 1000,
   typeSpeed: 60,
-=======
-	typeSpeed: 40,
-	backSpeed: 20,
->>>>>>> a640a1940be94fad7ae7fadd6bf95ddd78741ec9
+	backSpeed: 60,
   smartBackspace: true
 });
 
 $(function() {
-<<<<<<< HEAD
-  $(".right").hide(0).delay(10000).fadeIn(400);
-=======
-  $(".right").hide(0).delay(7125).fadeIn(400);
->>>>>>> a640a1940be94fad7ae7fadd6bf95ddd78741ec9
+  $(".right").hide(0).delay(12000).fadeIn(400);
   // $(".logo").hide(0).delay(7500).fadeIn(400);
 });
