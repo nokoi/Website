a640a1940be94fad7ae7fadd6bf95ddd78741ec9
Arne Swanson <atteinmos@0111.click>
2018-07-10 17:42:50 -0400

    made deleting animation a bit slower so the typing animation is a bit smoother  :)

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 var typed = new Typed("#typed", {
   stringsElement: "#typed-strings",
   strings: ["NOKOI is a game studio.", "NOKOI is a collective."],
-  typeSpeed: 40,
+	typeSpeed: 40,
+	backSpeed: 20,
   smartBackspace: true
 });
 
 $(function() {
-  $(".right").hide(0).delay(6500).fadeIn(400);
+  $(".right").hide(0).delay(7125).fadeIn(400);
   // $(".logo").hide(0).delay(7500).fadeIn(400);
 });
