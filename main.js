bc982e6070aaca05114f769d68166a904032531b
frogheck <frogheck@gmail.com>
2018-07-11 02:21:08 -0400

    added fade in to splash logo

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ const typed = new Typed("#typed", {
 	stringsElement: "#typed-strings",
 	typeSpeed: 40,
 	showCursor: false,
-	startDelay: 2200,
+	startDelay: 3000,
 	backSpeed: 20,
 	smartBackspace: true
 });
 
 $(() => {
-	$('#logo').delay(2000).fadeOut(200);
-	$('#content').hide(0).delay(2000).show(0);
-	$(".right").hide(0).delay(7125+2200+250).fadeIn(400);
+  $("#logo").hide().delay(500).fadeIn(200);
+	$("#logo").delay(2000).fadeOut(200);
+	$("#content").hide(0).delay(2500).show(0);
+	$(".right").hide(0).delay(7125+2200+250+500).fadeIn(400);
 });
